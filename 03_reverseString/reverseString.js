75d150be91e961d00876b949f7f570ff27a1afef
Richard Silva <akh3r0us1n@gmail.com>
2022-09-18 02:44:27 -0600

    Finished third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
 
+    let reversedString = '';
+    let index = sentence.length - 1;
+
+    while (index >= 0) {
+        reversedString += sentence[index];
+        --index;
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
