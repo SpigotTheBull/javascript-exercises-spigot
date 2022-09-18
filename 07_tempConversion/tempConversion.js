5c48511d929acf47b3bd9e28ad9586d0342e2872
Richard Silva <akh3r0us1n@gmail.com>
2022-09-18 13:49:14 -0600

    Finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenTemp) {
+  celsiusTemp = (fahrenTemp - 32) * (5/9);
+  celsiusTemp.toFixed(2);
+  celsiusTemp *= 10;
+  celsiusTemp = Math.round(celsiusTemp);
+  celsiusTemp /= 10;
+  return celsiusTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusTemp) {
+  fahrenTemp = (celsiusTemp * (9/5)) + 32;
+  fahrenTemp.toFixed(2);
+  fahrenTemp *= 10;
+  fahrenTemp = Math.round(fahrenTemp);
+  fahrenTemp /= 10;
+  return fahrenTemp;
 };
 
 // Do not edit below this line
