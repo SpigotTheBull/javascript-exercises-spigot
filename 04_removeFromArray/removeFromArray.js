e8cf5a8ad18860abe67c6cc90197930ab4a82ff9
Richard Silva <akh3r0us1n@gmail.com>
2022-09-18 03:30:27 -0600

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, choices) {
 
+    let cleanedArr = arr;
+
+    for (let i = 1; i < arguments.length; i++) {
+        const choice = arguments[i];
+
+        const elementExists = (element) => element === choice;
+
+        const foundIndex = cleanedArr.findIndex(elementExists);
+
+        if (foundIndex !== -1) {
+            cleanedArr.splice(foundIndex, 1);
+        }
+    }
+
+    return cleanedArr;
 };
 
 // Do not edit below this line
