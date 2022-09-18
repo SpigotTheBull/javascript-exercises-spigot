4644ee2972b173920d0b7941924807d2483aa00d
Richard Silva <akh3r0us1n@gmail.com>
2022-09-18 04:54:51 -0600

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(beginNumber, endNumber) {
 
+    if (!Number.isInteger(beginNumber) || !Number.isInteger(endNumber)) {
+        return 'ERROR';
+    }
+
+    if (beginNumber < 0 || endNumber < 0) {
+        return 'ERROR';
+    }
+
+    let finalSum = 0;
+
+    if (beginNumber <= endNumber) {
+        while (beginNumber <= endNumber) {
+            finalSum += beginNumber;
+            beginNumber += 1;
+        }
+    } else {
+        while (beginNumber >= endNumber) {
+            finalSum += beginNumber;
+            beginNumber -= 1;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
